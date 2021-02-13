5aa85894335c277334b89317298ebcdb93e86fa9
Gabriel Marques <gabriel_frahm@hotmail.com>
2021-02-13 16:34:36 -0300

    atualizando o dashProvider

--- web/src/pages/DashboardProvider/index.tsx
+++ web/src/pages/DashboardProvider/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
-// import { GiVikingChurch } from 'react-icons/gi';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+import { FiClock, FiPower } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../hooks/Auth';
-import { Container, Content, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Content,
+  Header,
+  HeaderContent,
+  Profile,
+  Schedule,
+  NextAppointment,
+  Calender,
+  Section,
+  Appointment,
+} from './styles';
 
 const DashboardProvider: React.FC = () => {
   const { user, signOut } = useAuth();
@@ -27,7 +39,75 @@ const DashboardProvider: React.FC = () => {
         </HeaderContent>
       </Header>
       <Content>
-        <h1>ola</h1>
+        <Schedule>
+          <h1>Atendimentos</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 13</span>
+            <span>Domingo</span>
+          </p>
+          <NextAppointment>
+            <strong>Cliente a seguir</strong>
+            <div>
+              <img src={user.avatar_url} alt={user.name} />
+              <strong>{user.name} Marques</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <div>
+                <img src={user.avatar_url} alt={user.name} />
+
+                <strong>{user.name}</strong>
+                <span>
+                  <FiClock />
+                  09:00
+                </span>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <div>
+                <img src={user.avatar_url} alt={user.name} />
+
+                <strong>{user.name}</strong>
+                <span>
+                  <FiClock />
+                  09:00
+                </span>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calender>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Setembro',
+              'Outubro',
+              'Novembro',
+              'Dezembro',
+            ]}
+          />
+        </Calender>
       </Content>
     </Container>
   );
--- web/src/pages/DashboardProvider/styles.ts
+++ web/src/pages/DashboardProvider/styles.ts
@@ -1,3 +1,4 @@
+import { shade } from 'polished';
 import styled from 'styled-components';
 
 export const Container = styled.div``;
@@ -81,4 +82,319 @@ export const Profile = styled.div`
   }
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 64px auto;
+  display: flex;
+
+  @media (max-width: 640px) {
+    margin: 20px auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+`;
+
+export const Schedule = styled.div`
+  @media (min-width: 300px) {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    h1 {
+      font-size: 30px;
+    }
+
+    p {
+      margin-top: 8px;
+      color: #b34d4b;
+      display: flex;
+      align-items: center;
+      font-weight: 500;
+
+      span {
+        display: flex;
+        align-items: center;
+      }
+
+      span + span::before {
+        content: '';
+        width: 5px;
+        height: 1px;
+        background: #b34d4b;
+        margin: 0 8px;
+      }
+    }
+  }
+
+  @media (min-width: 640px) {
+    margin-right: 80px;
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    h1 {
+      font-size: 36px;
+    }
+
+    p {
+      margin-top: 8px;
+      color: #b34d4b;
+      display: flex;
+      align-items: center;
+      font-weight: 500;
+
+      span {
+        display: flex;
+        align-items: center;
+      }
+
+      span + span::before {
+        content: '';
+        width: 5px;
+        height: 1px;
+        background: #b34d4b;
+        margin: 0 8px;
+      }
+    }
+  }
+`;
+
+export const NextAppointment = styled.div`
+  @media (min-width: 300px) {
+    margin-top: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    > strong {
+      color: #727272;
+      font-size: 20px;
+      font-weight: 300;
+    }
+
+    div {
+      background: #fff;
+      display: flex;
+      align-items: center;
+      padding: 16px 24px;
+      border-radius: 10px;
+      margin-top: 24px;
+      position: relative;
+
+      &::before {
+        position: absolute;
+        height: 80px;
+        width: 3px;
+        left: 0;
+        top: 2%;
+        content: '';
+        background: #b34d4b;
+      }
+
+      img {
+        width: 56px;
+        height: 56px;
+        border-radius: 50%;
+      }
+
+      strong {
+        margin-left: 50px;
+        margin-right: 24px;
+        color: #b34d4b;
+        font-size: 20px;
+      }
+
+      span {
+        margin-left: auto;
+        display: flex;
+        align-items: center;
+        color: #999591;
+
+        svg {
+          color: #b34d4b;
+          margin-right: 8px;
+        }
+      }
+    }
+  }
+`;
+
+export const ContentSection = styled.div`
+  /* @media (min-width: 640px) {
+    position: absolute;
+    bottom: 60px;
+  } */
+`;
+
+export const Section = styled.section`
+  display: block;
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    line-height: 26px;
+    border-bottom: 1px solid #3e3b47;
+    display: block;
+    padding-bottom: 16px;
+    margin-bottom: 16px;
+    margin-top: 16px;
+  }
+
+  > p {
+    color: #999591;
+  }
+
+  @media (max-width: 640px) {
+    > strong {
+      color: #999591;
+      font-size: 20px;
+      line-height: 26px;
+      border-bottom: 1px solid #3e3b47;
+      display: block;
+      padding-bottom: 16px;
+      margin-bottom: 16px;
+      margin-top: 16px;
+    }
+
+    > p {
+      color: #999591;
+    }
+  }
+`;
+
+export const Appointment = styled.div`
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 16px;
+  }
+
+  span {
+    /* margin-left: 12px; */
+    display: flex;
+    align-items: center;
+    color: #999591;
+    width: 70px;
+  }
+
+  svg {
+    color: #b34d4b;
+    margin-right: 8px;
+  }
+
+  div {
+    flex: 1;
+    background: #504642;
+    display: flex;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 10px;
+    /* margin-left: 24px; */
+  }
+
+  img {
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+  }
+
+  strong {
+    margin-left: 24px;
+    margin-right: 24px;
+    color: #fff;
+    font-size: 20px;
+  }
+`;
+
+export const Calender = styled.aside`
+  width: 300px;
+
+  @media (max-width: 640px) {
+    display: flex;
+
+    flex: 1;
+    margin-top: 20px;
+  }
+  .DayPicker {
+    border-radius: 10px;
+  }
+
+  .DayPicker-wrapper {
+    padding-bottom: 0;
+    background: #b34d4b;
+    border-radius: 10px;
+  }
+
+  .DayPicker,
+  .DayPicker-Month {
+    width: 100%;
+  }
+
+  .DayPicker-NavButton {
+    color: #999591 !important;
+  }
+
+  .DayPicker-NavButton--prev {
+    right: auto;
+    left: 1.5em;
+    margin-right: 0;
+  }
+
+  .DayPicker-Month {
+    border-collapse: separate;
+    border-spacing: 8px;
+    margin: 16px 0 0 0;
+    padding: 8px;
+    background-color: #fff;
+    border-radius: 0 0 10px 10px;
+    color: #666360;
+  }
+
+  .DayPicker-Caption {
+    margin-bottom: 1em;
+    padding: 0 1em;
+    color: #f4ede8;
+
+    > div {
+      text-align: center;
+    }
+  }
+
+  .DayPicker-Day {
+    width: 40px;
+    height: 40px;
+  }
+
+  .DayPicker-Day--available:not(.DayPicker-Day--outside) {
+    background: #b34d4b;
+    border-radius: 10px;
+    color: #fff;
+  }
+
+  .DayPicker:not(.DayPicker--interactionDisabled)
+    .DayPicker-Day:not(.DayPicker-Day--disabled):not(.DayPicker-Day--selected):not(.DayPicker-Day--outside):hover {
+    background: ${shade(0.2, '#b34d4b')};
+  }
+
+  .DayPicker-Day--today {
+    font-weight: normal;
+  }
+
+  .DayPicker-Day--disabled {
+    color: #666360 !important;
+    background: transparent !important;
+  }
+
+  .DayPicker-Day--selected {
+    background: #b34d4b !important;
+    border-radius: 10px;
+    color: #232129 !important;
+  }
+`;
