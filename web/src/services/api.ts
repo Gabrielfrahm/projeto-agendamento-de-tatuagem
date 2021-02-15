60be55ee23023ad1d1f1412362232b5ed6fbd7b1
Gabriel Marques <gabriel_frahm@hotmail.com>
2021-02-15 14:09:05 -0300

    Atualizacao de perfil

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
-  // baseURL: 'https://agendamento-tattoo.herokuapp.com',
+  // baseURL: 'http://localhost:3333',
+  baseURL: 'https://agendamento-tattoo.herokuapp.com',
 });
 
 export default api;
